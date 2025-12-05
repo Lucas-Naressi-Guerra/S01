1622ce3c1347ecd71271887fb6275fbfb384f7f1
Lucas-Naressi-Guerra <lucasnaressiguerra@gmail.com>
2025-12-05 12:38:00 -0300

    Relatório 15 - Disciplinas de Alto Valor.pl

---
+++ Relalatório 15 - Disciplinas de Alto Valor.pl
@@ -0,0 +1,59 @@
+% Fatos: Disciplinas (nome, créditos, area)
+disciplina(programacao1, 6, fundamental).
+disciplina(calculo1, 8, matematica).
+disciplina(estrutura_dados, 6, computacao).
+disciplina(algoritmos, 4, computacao).
+disciplina(banco_dados, 6, aplicacoes).
+disciplina(redes, 4, aplicacoes).
+disciplina(inteligencia_artificial, 8, computacao).
+disciplina(compiladores, 6, computacao).
+disciplina(teoria_grafos, 4, matematica).
+disciplina(estatistica, 6, matematica).
+disciplina(sistemas_operacionais, 6, computacao).
+disciplina(projeto_integrador, 10, aplicacoes).
+
+% Fatos: Pré-Requisitos (disciplina_principal, pre_requisito)
+prerequisito(estrutura_dados, programacao1).
+prerequisito(algoritmos, estrutura_dados).
+prerequisito(banco_dados, programacao1).
+prerequisito(inteligencia_artificial, algoritmos).
+prerequisito(inteligencia_artificial, calculo1).
+prerequisito(compiladores, algoritmos).
+prerequisito(compiladores, estrutura_dados).
+prerequisito(sistemas_operacionais, estrutura_dados).
+prerequisito(teoria_grafos, calculo1).
+prerequisito(estatistica, calculo1).
+prerequisito(projeto_integrador, banco_dados).
+prerequisito(projeto_integrador, sistemas_operacionais).
+
+% Fatos: Conhecimento de Alunos
+concluiu(maria, programacao1).
+concluiu(maria, calculo1).
+concluiu(joao, programacao1).
+concluiu(joao, estrutura_dados).
+concluiu(joao, calculo1).
+concluiu(ana, programacao1).
+concluiu(ana, redes).
+concluiu(ana, teoria_grafos).
+concluiu(pedro, calculo1).
+concluiu(pedro, estatistica).
+concluiu(pedro, teoria_grafos).
+concluiu(lucas, estrutura_dados).
+concluiu(lucas, algoritmos).
+concluiu(lucas, redes).
+concluiu(lucas, banco_dados).
+
+
+
+% 1. alto_impacto(D)
+alto_impacto(D) :-
+    disciplina(D, Cred, _),
+    Cred > 5,
+    prerequisito(D1, D),
+    prerequisito(D2, D),
+    D1 \= D2.
+
+% 2. Query: disciplinas de matemática com 4 créditos e sem serem pré-requisito
+disciplina_valiosa(D) :-
+    disciplina(D, 4, matematica),
+    \+ prerequisito(_, D).
